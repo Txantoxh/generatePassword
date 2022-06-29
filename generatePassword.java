49335bacdd12764c1d2fcd5ba681ce663a2f43e9
SantoshMThakuri <iamsantoshmthakuri@gmail.com>
2022-06-29 13:21:03 -0400

    passwordgenerate

--- generatePassword.java
+++ generatePassword.java
@@ -2,22 +2,20 @@ import java.util.*;
 import java.util.Scanner;
 
 public class generatePassword{
-//public static int length;
-static Scanner input= new Scanner(System.in);
-
     public static void main(String[]args){
-        int Numbers;
         int answer;
         int length;
-        System.out.println(" Enter the number");
+        System.out.println(" Enter the two digits numbers");
+        
         
         Scanner scan = new Scanner(System.in);
-        length = Scanner.nextInt();
+        length = scan.nextInt();
         
         String CapitalCaseLetter = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
         String LowerCaseLetter = "abcdefghijklmnopqrstuvwxyz";
         String SpecialCharacters ="@#$";
+        String Numbers ="1234567890";
 
         String CombinedChar = CapitalCaseLetter + LowerCaseLetter +SpecialCharacters + Numbers;
         Random rand = new Random(); // generate random numbers.
@@ -27,11 +25,11 @@ static Scanner input= new Scanner(System.in);
         password[0] = LowerCaseLetter.charAt(rand.nextInt(LowerCaseLetter.length()));
         password[1] = CapitalCaseLetter.charAt(rand.nextInt(CapitalCaseLetter.length()));
         password[2] = SpecialCharacters.charAt(rand.nextInt(SpecialCharacters.length()));
-        password[3] = SpecialCharacters.charAt(rand.nextInt(SpecialCharacters.length()));
+        password[3] = Numbers.charAt(rand.nextInt(Numbers.length()));
 
-        for(int i = 1;i<length;i++);
+        for(int i = 0;i<length;i++)
         {
-            password[i]= CombinedChar.charAt(rand.nextInt(CombinedChar.length()));
+        password[i]= CombinedChar.charAt(rand.nextInt(CombinedChar.length()));
         }
         System.out.println(password);
       
